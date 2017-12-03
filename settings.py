38aa152d226cdefb98e7b3b79abc846c01946837
Luv <luv.singh@crevise.com>
2017-12-03 13:15:30 +0530

    Adds error check

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ import logging
 
 config = {
     "crawler_type": "disk",
-    "base_path": "C:\\Users\\38803\\Work\\Bhola",
+    "base_path": "/home/luv/Downloads/",
     "log_level": logging.DEBUG
 }
